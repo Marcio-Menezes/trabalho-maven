30e794bcaf1ecd723dc24b4564b63189404d7c9a
burninha <viecelib@gmail.com>
2021-04-06 19:24:02 -0300

    Att

--- src/main/java/br/pucrs/ep/marciomenezes/App.java
+++ src/main/java/br/pucrs/ep/marciomenezes/App.java
@@ -38,10 +38,10 @@ public class App {
                 System.out.println("Marca : " + marca);
                 System.out.println("Cor : " + cor);
                 System.out.println("Categoria : " + categoria);
-                System.out.println("-------------------\n\n");
+                System.out.println("----------------------\n\n");
 
 
-                
+
 
 
             }
